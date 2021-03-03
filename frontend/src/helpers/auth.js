99b3b3315dde887ab3dde89d3823996e352bba67
Vianney RIOTTE <vianney.riotte@outlook.com>
2021-03-03 11:41:48 +0100

    Pb hot reload windows docker

--- frontend/src/helpers/auth.js
+++ frontend/src/helpers/auth.js
@@ -1,11 +1,23 @@
 import storage from '../helpers/storage';
 import http from '../helpers/http';
 
-const connexion = async ({ email, password }) => {
+const connexion = async ({
+	email,
+	password
+}) => {
 	try {
-		const result = await http.doPost({ url: 'auth/login', data: { email, password }, secured: false });
+		const result = await http.doPost({
+			url: 'auth/login',
+			data: {
+				email,
+				password
+			},
+			secured: false
+		});
 		if (result.ok && result.data && result.data.token) {
-			await storage.setUser({ email });
+			await storage.setUser({
+				email
+			});
 			await storage.setToken(result.data.token);
 			return true;
 		} else {
@@ -18,7 +30,9 @@ const connexion = async ({ email, password }) => {
 
 const getUserInfo = async () => {
 	try {
-		const result = await http.doGet({ url: 'secured/userinfo' });
+		const result = await http.doGet({
+			url: 'secured/userinfo'
+		});
 		if (result.ok && result.data) {
 			return result.data;
 		} else {
@@ -29,4 +43,7 @@ const getUserInfo = async () => {
 	}
 };
 
-export default { connexion, getUserInfo };
+export default {
+	connexion,
+	getUserInfo
+};
